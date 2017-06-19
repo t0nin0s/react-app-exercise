2b6a788d9f5a93e08b2aabaa5f691e18f3e77c69
Richard Moss <richard@englishtutorslive.com>
2017-06-19 17:31:00 +0100

    Adds component for routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,93 +1,18 @@
-import React, { Component } from 'react'
-import AppBar from 'material-ui/AppBar'
-import { pink700 } from 'material-ui/styles/colors'
-import NavDrawer from './layout/NavDrawer'
-import { typography } from 'material-ui/styles'
-import withWidth, { LARGE } from 'material-ui/utils/withWidth'
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 
-import UserListContainer from './user/UserListContainer'
-import WorkshopListContainer from './workshop/WorkshopListContainer'
-import Dashboard from './dashboard'
 import NotFound from './NotFound'
 import Login from './Login'
+import Main from './Main'
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      nav: { open: false }
-    }
-    this.toggleNav = this.toggleNav.bind(this)
-    this.closeNav = this.closeNav.bind(this)
-  }
-
-  getStyles() {
-    const styles = {
-      appBar: {
-        position: 'fixed',
-        top: 0,
-        color: typography.textFullWhite
-      },
-      app: {},
-      logo: {
-        cursor: 'pointer',
-        fontSize: 24,
-        color: typography.textFullWhite,
-        fontWeight: typography.fontWeightLight,
-        backgroundColor: pink700
-      }
-    }
-
-    return styles
-  }
-
-  toggleNav() {
-    this.setState({ nav: { open: !this.state.nav.open } })
-  }
-
-  closeNav() {
-    this.setState({ nav: { open: false } })
-  }
-
-  render() {
-    let docked = false
-    let navDrawerOpen = this.state.nav.open
-    let styles = this.getStyles()
-
-    if (this.props.width === LARGE) {
-      docked = true
-      navDrawerOpen = true
-      styles.app.paddingLeft = 256
-    }
-
+const App = () => {
     return (
-      <div className="main-view">
-        <AppBar
-          title="Course Manager"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-          onLeftIconButtonTouchTap={this.toggleNav}
-          className="app-bar"
-        />
-        <NavDrawer
-          open={navDrawerOpen}
-          toggleNav={this.toggleNav}
-          closeNav={this.closeNav}
-          styles={styles}
-          docked={docked}
-        />
-        <div className="view-container" style={styles.app}>
-          <Switch>
-            <Route exact path="/" component={Dashboard} />
-            <Route path="/users" component={UserListContainer} />
-            <Route path="/login" component={Login} />
-            <Route path="/workshops" component={WorkshopListContainer} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </div>
+      <Switch>
+        <Route exact path="/" component={Main} />
+        <Route path="/login" component={Login} />
+        <Route component={NotFound} />
+      </Switch>
     )
   }
-}
 
-export default withWidth()(App)
+export default App
