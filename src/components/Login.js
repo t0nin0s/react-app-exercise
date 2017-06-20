382f6ea7fe9fab383eed0c7f3168e3388c785f04
Richard Moss <richard@englishtutorslive.com>
2017-06-20 16:43:09 +0100

    commited point 7

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,35 @@
 import React, {Component} from 'react'
 import {FormGroup, FormControl, Button} from 'react-bootstrap'
+import { withRouter } from 'react-router-dom'
 
 class Login extends Component {
+  constructor () {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+handleChange = (name, event) => {
+    let change = {}
+    change[name] = event.target.value
+    this.setState(change)
+  }
+
+handleSubmit = (e) => {
+  e.preventDefault()
+  const { history } = this.props
+
+  if (this.state.username === 'reactboy' &&
+        this.state.password === '1234') {
+
+      localStorage.setItem('isAuthenticated', 1)
+
+      history.push('/')
+    }
+}
+
   render () {
     return (
       <form className="form-signin">
@@ -10,14 +38,26 @@ class Login extends Component {
         </FormGroup>
 
         <FormGroup>
-          <FormControl className="form-control" id="email" type="email" value="" placeholder="Enter email" />
-          <FormControl className="form-control" id="password" type="password" value="" placeholder="Password" />
+          <FormControl
+            className="form-control"
+            id="email"
+            type="email"
+            value={this.state.username}
+            onChange={this.handleChange.bind(this, 'username')}
+            placeholder="Enter email" />
+          <FormControl
+            className="form-control"
+            id="password"
+            type="password"
+            value={this.state.password}
+            onChange={this.handleChange.bind(this, 'password')}
+            placeholder="Password" />
         </FormGroup>
 
-        <Button bsSize="large" bsStyle="primary" block type="submit">Sign in</Button>
+        <Button bsSize="large" bsStyle="primary" block type="submit" onClick={this.handleSubmit}>Sign in</Button>
       </form>
     )
   }
 }
 
-export default Login
+export default withRouter(Login)
